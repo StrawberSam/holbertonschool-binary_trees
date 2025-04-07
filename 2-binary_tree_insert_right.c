291f10bc65dae4ca70546a843be3c526f9f73f98
StrawberSam <s.roche53@hotmail.com>
2025-04-07 15:33:05 +0200

    Fix comments for betty style

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Insère un nœud à gauche d’un parent donné
- * @parent: Pointeur vers le nœud parent où insérer le nœud gauche
+ * binary_tree_insert_right - Insère un nœud à droite d’un parent donné
+ * @parent: Pointeur vers le nœud parent où insérer le nœud droit
  * @value: Valeur à stocker dans le nouveau nœud
  *
  * Return: Pointeur vers le nouveau nœud, ou NULL si échec
@@ -19,14 +19,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	/*Si le parent a déjà un enfant gauche, on déplace sous le nouveau nœud*/
+	/*Si le parent a déjà un enfant droit, on déplace sous le nouveau nœud*/
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
 
-	/* Le nouveau nœud devient l'enfant gauche du parent */
+	/* Le nouveau nœud devient l'enfant droit du parent */
 	parent->right = new_node;
 
 	return (new_node);
