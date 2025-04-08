9d2b9e6bf9487d68bf818f063a5b24248289144d
GuarickGit <10615@holbertonstudents.com>
2025-04-08 09:49:18 +0200

    update betty description and comments

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,11 +1,21 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Description: Recursively frees the nodes of a binary tree
+ * using post-order traversal (left, right, root).
+ * If tree is NULL, does nothing.
+ */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
+	/* Si tree est NULL, on s'arrête */
 	if (tree == NULL)
 		return;
 
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->right);
-	free(tree);
+	binary_tree_delete(tree->left); /* on parcourt le sous-arbre de gauche */
+	binary_tree_delete(tree->right); /* on parcourt le sous-arbre de droite */
+	free(tree); /* on free tree */
 }
